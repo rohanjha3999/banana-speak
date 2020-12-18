71a2d1d3e57f2da0bec3f0ca916950cb45293683
rohanjha3999 <rohanjha3999@gmail.com>
2020-12-18 09:27:54 +0530

    url

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 var btnTranslate = document.querySelector("#btn-Translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+// var serverURL = "https://rohankumar.netlify.app/?text=I am Rohan jha"
+
+// function getTranslateURL(text){
+//     return serverURL + "?" + "text" + text
+// }
+
+function clickHandler() {
+        // outputDiv.innerText = "ajsjsjsjs " + txtInput.Value;
+
+        var inpurtText = txtInput.Value; 
+
+        // fetch(getTranslateURL(text))
+        // .then()
+};
+btnTranslate.addEventListener("click",clickHandler());
+
+
