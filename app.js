fdf5a66fb7d2dfe991996da76c8cbf20588312ac
rohanjha3999 <rohanjha3999@gmail.com>
2020-12-18 10:26:10 +0530

    image

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
-var btnTranslate = document.querySelector("#btn-Translate");
+var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-// var serverURL = "https://rohankumar.netlify.app/?text=I am Rohan jha"
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-// function getTranslateURL(text){
-//     return serverURL + "?" + "text" + text
-// }
+var serverURL = "https://api.funtranslations.com/translate/groot.json"
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
 
 function clickHandler() {
-        // outputDiv.innerText = "ajsjsjsjs " + txtInput.Value;
+    var inputText = txtInput.value; // taking input
 
-        var inpurtText = txtInput.Value; 
-
-        // fetch(getTranslateURL(text))
-        // .then()
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
 };
-btnTranslate.addEventListener("click",clickHandler());
+
+btnTranslate.addEventListener("click", clickHandler)
 
 
